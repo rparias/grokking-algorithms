8a82e9368bdc03e8abaa1e5b15150abf5d515647
Ronald Arias <ariasron@hotmail.com>
2022-01-15 17:39:06 -0500

    Binary search

--- src/01-binary-search/binarySearch.js
+++ src/01-binary-search/binarySearch.js
@@ -1,5 +1,27 @@
-const binarySearch = () => {
-  return null
+const binarySearch = (list, element) => {
+  let low = 0
+  let high = list.length - 1
+
+  while (low <= high) {
+    let middle = getMiddle(low, high)
+    const guess = list[middle]
+
+    if (guess === element) {
+      return true
+    }
+
+    if (guess > element) {
+      high = middle - 1
+    } else {
+      low = middle + 1
+    }
+  }
+
+  return false
+}
+
+const getMiddle = (low, high) => {
+  return Math.floor((low + high) / 2)
 }
 
 module.exports = binarySearch
--- src/01-binary-search/test/binarySearch.spec.js
+++ src/01-binary-search/test/binarySearch.spec.js
@@ -1,12 +1,21 @@
 const binarySearch = require('../binarySearch')
 
 describe('binary search', () => {
-  it('should return null when an element is not found', () => {
+  it('should return false when an element is not found', () => {
     const myList = [1, 3, 5, 7, 9]
     const elementToFind = 4
 
     const element = binarySearch(myList, elementToFind)
 
-    expect(element).toBeNull()
+    expect(element).toBe(false)
+  })
+
+  it('should return true when an element is found in the list', () => {
+    const myList = [1, 3, 5, 7, 9]
+    const elementToFind = 1
+
+    const element = binarySearch(myList, elementToFind)
+
+    expect(element).toBe(true)
   })
 })
